cfab2dd89e2ef7fa907553dd3959109a7a4ed1e1
David Blood <9371095+davidbjourno@users.noreply.github.com>
2019-07-12 18:04:37 +0100

    Implement react-intersection-observer in <LineChart>

--- client/components/line-chart/index.js
+++ client/components/line-chart/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useInView } from 'react-intersection-observer';
 
-const LineChart = () => (
-  <h2>
-Line chart
-  </h2>
-);
+const LineChart = () => {
+  const [chartRef, inView] = useInView({ threshold: 1 });
+
+  if (inView) {
+    console.log('Line chart in view'); // eslint-disable-line no-console
+  }
+
+  return (
+    <div ref={chartRef} className="line-chart__container">
+      <h2>
+        {`Line chart in view: ${inView}`}
+      </h2>
+    </div>
+  );
+};
 
 export default LineChart;
