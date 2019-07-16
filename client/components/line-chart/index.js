b7ad5b1f6b90830537749a7266c246cc0985ecdf
Jane Pong <officeofjane@gmail.com>
2019-07-16 13:57:03 +0800

    set up basic axes and lines

--- client/components/line-chart/index.js
+++ client/components/line-chart/index.js
@@ -5,15 +5,28 @@ import * as d3 from 'd3';
 
 const margin = {
   top: 20,
-  right: 20,
-  bottom: 20,
-  left: 20,
+  right: 105,
+  bottom: 30,
+  left: 50,
 };
-const x = d3.scaleLinear();
+
+const HIGHLIGHT = 'Remittances';
+
+const parseDate = d3.timeParse('%Y');
+
+const x = d3.scaleTime();
 const y = d3.scaleLinear();
-const xAxis = d3.axisBottom(x);
-const yAxis = d3.axisLeft(y);
-const line = d3.line();
+const colour = d3.scaleOrdinal(d3.schemeCategory10);
+
+const xAxis = d3.axisBottom()
+  .scale(x)
+  .ticks(5);
+const yAxis = d3.axisLeft()
+  .scale(y)
+  .ticks(10);
+
+const line = d3.line()
+  .curve(d3.curveMonotoneX);
 let pathDefinition;
 
 const LineChart = (props) => {
@@ -23,24 +36,37 @@ const LineChart = (props) => {
   const xAxisRef = useRef(null);
   const yAxisRef = useRef(null);
 
-  console.log(testdata);
+  const keys = d3.keys(testdata[0]).filter(key => key != 'year');
+  colour.domain(keys);
+
+  const nestedData = keys.map(name => {
+    return {
+      name,
+      label: name.replace(/_/g, ' '),
+      values: testdata.map(d => {
+        return {date: parseDate(d.year), value: +d[name]};
+      })
+    }
+  })
 
   // Draw chart (run only on change to width or height prop)
   useEffect(() => {
     // Configure scales
-    x.domain([0, data.length - 1]).range([margin.left, width - margin.right]);
-    y.domain([0, d3.max(data)]).range([height - margin.bottom, margin.top]);
+    x.domain(d3.extent(testdata, d => parseDate(d.year)))
+      x.range([margin.left, width - margin.right]);
+    y.domain([
+      d3.min(nestedData, c => d3.min(c.values, v => v.value)),
+      d3.max(nestedData, c => d3.max(c.values, v => v.value))
+    ]).nice()
+      .range([height - margin.bottom, margin.top]);
 
-    // Configure axes
-    xAxis.ticks(data.length - 1);
-
-    // Configure line generator and generate path definition
-    line.x((d, i) => x(i)).y(d => y(d));
-    pathDefinition = line(data);
+    // Configure line generator
+    line.x(d => x(d.date))
+      .y(d => y(d.value));
 
     // Let D3 render axes
-    d3.select(xAxisRef.current).call(xAxis);
-    d3.select(yAxisRef.current).call(yAxis);
+    d3.select(xAxisRef.current).call(xAxis).select('.domain').remove();
+    d3.select(yAxisRef.current).call(yAxis).select('.domain').remove();
   }, [width, height]);
 
   if (inView) {
@@ -49,7 +75,30 @@ const LineChart = (props) => {
 
   return (
     <div ref={containerRef} className="line-chart__container">
+      <h2>
+        {`Line chart 100% in view: ${inView}`}
+      </h2>
 
+      <svg ref={svgRef} viewBox={`0 0 ${width} ${height}`}>
+        <g ref={xAxisRef} transform={`translate(0, ${height - margin.bottom})`} />
+        <g ref={yAxisRef} transform={`translate(${margin.left}, 0)`} />
+        { nestedData.map((d) => {
+          // generate path definition
+          pathDefinition = line(d.values);
+          return (
+            <path
+              key={d.name}
+              d={pathDefinition}
+              fill="none"
+              stroke="steelblue"
+              strokeWidth={2}
+              strokeLinejoin="round"
+              strokeLinecap="round"
+            />
+          )
+        })
+        }
+      </svg>
     </div>
   );
 };
