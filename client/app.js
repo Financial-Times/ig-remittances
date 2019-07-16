058c19a7b04a8890f390d84f3dca6cbdfb7c5c17
Jane Pong <officeofjane@gmail.com>
2019-07-16 16:19:02 +0800

    add line labels + styling

--- client/app.js
+++ client/app.js
@@ -56,7 +56,7 @@ import { ContextPropType, ContextDefaultProps } from './util/prop-types';
 import FullBleedTopper from './components/full-bleed-topper';
 import LineChart from './components/line-chart';
 import ChordDiagram from './components/chord-diagram';
-import testdata from '../data/remittances-line.csv';
+import lineChartData from '../data/remittances-line.csv';
 
 const App = (context) => {
   // This sets the initial state of the application. We need
@@ -73,8 +73,6 @@ const App = (context) => {
   }, []);
 
   const { data } = state; // eslint-disable-line no-unused-vars
-  const lineChartData = [0, 6, 4, 10];
-
 
   // console.dir(data); // eslint-disable-line no-console
 
@@ -96,7 +94,7 @@ const App = (context) => {
               multa ok, sur ðū īnfāno kæŭze. Om ene modō sekvanta proksimumecō, ānÞ sh tiele hiper defīnītive.
             </p>
 
-            <LineChart data={lineChartData} testdata={testdata} width={700} height={500} />
+            <LineChart data={lineChartData} width={700} height={500} />
 
             <p>
               Nk sola ēsperanÞiġo obl, mulÞō ipsilono nēdifīnita ien ed. Trīliono kōmpleksa co mil, kī āġā farī onin
--- client/components/line-chart/index.js
+++ client/components/line-chart/index.js
@@ -5,9 +5,9 @@ import * as d3 from 'd3';
 
 const margin = {
   top: 20,
-  right: 105,
-  bottom: 30,
-  left: 50,
+  right: 130,
+  bottom: 50,
+  left: 35,
 };
 
 const HIGHLIGHT = 'Remittances';
@@ -30,20 +30,20 @@ const line = d3.line()
 let pathDefinition;
 
 const LineChart = (props) => {
-  const { data, testdata, width, height } = props;
+  const { data, width, height } = props;
   const [containerRef, inView] = useInView({ threshold: 1, triggerOnce: true });
   const svgRef = useRef(null);
   const xAxisRef = useRef(null);
   const yAxisRef = useRef(null);
 
-  const keys = d3.keys(testdata[0]).filter(key => key != 'year');
+  const keys = d3.keys(data[0]).filter(key => key != 'year');
   colour.domain(keys);
 
   const nestedData = keys.map(name => {
     return {
       name,
       label: name.replace(/_/g, ' '),
-      values: testdata.map(d => {
+      values: data.map(d => {
         return {date: parseDate(d.year), value: +d[name]};
       })
     }
@@ -52,7 +52,7 @@ const LineChart = (props) => {
   // Draw chart (run only on change to width or height prop)
   useEffect(() => {
     // Configure scales
-    x.domain(d3.extent(testdata, d => parseDate(d.year)))
+    x.domain(d3.extent(data, d => parseDate(d.year)))
       x.range([margin.left, width - margin.right]);
     y.domain([
       d3.min(nestedData, c => d3.min(c.values, v => v.value)),
@@ -82,22 +82,42 @@ const LineChart = (props) => {
       <svg ref={svgRef} viewBox={`0 0 ${width} ${height}`}>
         <g ref={xAxisRef} transform={`translate(0, ${height - margin.bottom})`} />
         <g ref={yAxisRef} transform={`translate(${margin.left}, 0)`} />
-        { nestedData.map((d) => {
+        <line x1={margin.left} x2={width - margin.right} y1={y(0)} y2={y(0)} fill='none' stroke='#000' strokeWidth='1px' shapeRendering='crispEdges' strokeDasharray='3, 3' />
+
+        { inView && nestedData.map((d) => {
           // generate path definition
           pathDefinition = line(d.values);
+
+          // line label placement
+          let labelX = x(d.values[d.values.length -1].date);
+          let labelY = y(d.values[d.values.length -1].value);
+
+          let currentColour = colour(d.name);
+
           return (
-            <path
-              key={d.name}
-              d={pathDefinition}
-              fill="none"
-              stroke="steelblue"
-              strokeWidth={2}
-              strokeLinejoin="round"
-              strokeLinecap="round"
-            />
+            <g className='line' key={d.name}>
+              <path
+                d={pathDefinition}
+                fill="none"
+                stroke={currentColour}
+                strokeWidth={d.name == HIGHLIGHT ? '2.5px' : '2px'}
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                opacity={d.name == HIGHLIGHT ? 1 : 0.5}
+                />
+              <text
+                className='line-label'
+                transform={`translate(${labelX}, ${labelY})`}
+                x={5}
+                dy={d.name == HIGHLIGHT ? '-.2em' : '.35em'}
+                fill={currentColour}
+                fontWeight={d.name == HIGHLIGHT ? 600 : 400}
+              >
+                {d.label}
+              </text>
+            </g>
           )
-        })
-        }
+        })}
       </svg>
     </div>
   );
