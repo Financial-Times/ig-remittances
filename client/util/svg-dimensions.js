7300b590d3db3c9a15536eee450bbdb6da274f1e
David Blood <9371095+davidbjourno@users.noreply.github.com>
2019-08-13 15:42:41 +0100

    Changes from PR review

--- client/util/svg-dimensions.js
+++ client/util/svg-dimensions.js
@@ -1,15 +1,13 @@
 const svgDimensions = (width) => {
-  let dimensions;
-
   if (width >= 1220) {
-    dimensions = { width: 1180, height: 700 };
-  } else if (width >= 740) {
-    dimensions = { width: 700, height: 500 };
-  } else {
-    dimensions = { width: 300, height: 400 };
+    return { width: 1180, height: 700 };
   }
 
-  return dimensions;
+  if (width >= 740) {
+    return { width: 700, height: 500 };
+  }
+
+  return { width: 300, height: 400 };
 };
 
 export default svgDimensions;
