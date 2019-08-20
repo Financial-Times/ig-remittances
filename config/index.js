fe41540386d6605694900812c1b0b10073c7ad9d
David Blood <9371095+davidbjourno@users.noreply.github.com>
2019-08-20 18:32:43 +0100

    First pass

--- config/index.js
+++ config/index.js
@@ -5,11 +5,26 @@
  */
 
 import axios from 'axios';
+import StructuredGoogleDocsClient from 'structured-google-docs-client';
 import article from './article';
 import getFlags from './flags';
 import getOnwardJourney from './onward-journey';
 
 const BILATERAL_BERTHA = 'http://bertha.ig.ft.com/view/publish/gss/1CDRVnjEDTV7eXnYxM0FoabpC9O6LlyyMci1lzLmWX2w/total,bilateral';
+const COPY_BERTHA = '12TyuigeVMv5ut8YW-qVsQdtkLKraH0t43BqBFLfu4Rk';
+async function loadCopy(id) {
+  const content = await StructuredGoogleDocsClient(id);
+  const contentClean = content
+    .replace('<html><head></head><body>', '') // remove top guff
+    .replace('</body></html>', '') // remove bottom guff
+    .replace('<p></p>', '') // remove empty paragraphs
+    .replace(/<\/?u>/g, '') // links seem to come with <u> tags too
+    .replace(/<p>/g, '')
+    .split(/<\/p>/g);
+  const cruft = contentClean.pop(); // eslint-disable-line no-unused-vars
+
+  return contentClean;
+}
 
 export default async (environment = 'development') => {
   const articleData = await article(environment);
@@ -18,10 +33,10 @@ export default async (environment = 'development') => {
   const {
     data: { bilateral: bilateralData, total: gdps },
   } = await axios(BILATERAL_BERTHA);
-  console.dir(bilateralData);
   const countries = new Map(
     bilateralData.filter(d => d.source === 'WORLD').map(({ target, source, ...d }) => [target, { ...d, children: [] }]),
   );
+  const copy = await loadCopy(COPY_BERTHA);
 
   bilateralData
     .filter(({ source }) => source !== 'WORLD')
@@ -53,5 +68,6 @@ export default async (environment = 'development') => {
         return null;
       })
       .filter(d => d),
+    copy,
   };
 };
