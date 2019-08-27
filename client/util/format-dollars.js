af95861f3e171e7bdb5c623e8d14519f90db43de
Ændrew Rininsland <andrew.rininsland@ft.com>
2019-08-27 13:59:46 +0100

    Forgot to add formatter util

---
+++ client/util/format-dollars.js
@@ -0,0 +1,9 @@
+export const formatDollars = value => (value > 1e9
+  ? `${(value / 1e9).toLocaleString('en', {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  })}bn`
+  : `${(value / 1e6).toLocaleString('en', {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  })}m`);
