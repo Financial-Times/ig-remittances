8b7cd543be13652fb9327b8a028397ec84d35908
Ændrew Rininsland <andrew.rininsland@ft.com>
2019-08-27 14:09:01 +0100

    More formatting fun

--- client/util/format-dollars.js
+++ client/util/format-dollars.js
@@ -1,7 +1,7 @@
 export const formatDollars = value => (value > 1e9
   ? `${(value / 1e9).toLocaleString('en', {
     minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
+    maximumFractionDigits: 1,
   })}bn`
   : `${(value / 1e6).toLocaleString('en', {
     minimumFractionDigits: 0,
